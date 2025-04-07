40dbff8bf2d4d327018955de8dad8679753f7ac6
Zoey Hamm <339622581@GoTVDSB.ca>
2025-04-07 14:40:30 -0400

    Finished Tutorial and Challenges. No assignment

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Monogame_3___Loops__Lists__Input
 {
@@ -8,6 +10,18 @@ namespace Monogame_3___Loops__Lists__Input
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        KeyboardState keyboardState;
+        Rectangle mowerRect, window;
+        Texture2D grassTexture, mowerTexture;
+
+        SoundEffect mowerSound;
+        SoundEffectInstance mowerSoundInstance;
+
+        Vector2 mowerSpeed;
+
+        List<Rectangle> grassTiles;
+
 
         public Game1()
         {
@@ -18,7 +32,22 @@ namespace Monogame_3___Loops__Lists__Input
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 600, 500);
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.ApplyChanges();
+
+            mowerRect = new Rectangle(100,100,30,30);
+
+            grassTiles = new List<Rectangle>();
+            for (int x = 0; x < window.Width; x += 2)
+            {
+                for (int y = 0; y < window.Height; y += 2)
+                {
+                    grassTiles.Add(new Rectangle(x, y, 2, 2));
+                }
+            }
+
 
             base.Initialize();
         }
@@ -27,15 +56,70 @@ namespace Monogame_3___Loops__Lists__Input
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            mowerTexture = Content.Load<Texture2D>("Images/mower");
+            grassTexture = Content.Load<Texture2D>("Images/long_grass");
+            mowerSound = Content.Load<SoundEffect>("Sounds/mower_sound");
+            mowerSoundInstance = mowerSound.CreateInstance();
+            mowerSoundInstance.IsLooped = true;
         }
 
         protected override void Update(GameTime gameTime)
         {
+            keyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            mowerSpeed = Vector2.Zero;
+            if (keyboardState.IsKeyDown(Keys.W))
+                mowerSpeed.Y -= 5;
+            if (keyboardState.IsKeyDown(Keys.S))
+                mowerSpeed.Y += 5;
+            if (keyboardState.IsKeyDown(Keys.A))
+                mowerSpeed.X -= 5;
+            if (keyboardState.IsKeyDown(Keys.D))
+                mowerSpeed.X += 5;
 
-            // TODO: Add your update logic here
+            mowerRect.Offset(mowerSpeed);
+
+            for (int i = 0; i < grassTiles.Count; i++)
+            {
+                if (mowerRect.Contains(grassTiles[i]))
+                {
+                    grassTiles.Remove(grassTiles[i]);
+                    i--;
+                }
+            }
+
+            if (mowerRect.X < 0)
+            {
+                mowerRect.X = 0;
+            }
+            if (mowerRect.X > 570)
+            {
+                mowerRect.X = 570;
+            }
+            if (mowerRect.Y < 0)
+            {
+                mowerRect.Y = 0;
+            }
+            if (mowerRect.Y > 470)
+            {
+                mowerRect.Y = 470;
+            }
+
+            if (mowerSpeed == Vector2.Zero)
+            {
+                mowerSoundInstance.Stop();
+            }
+            else
+            {
+                mowerSoundInstance.Play();
+            }
+
+            if (grassTiles.Count == 0) // Doesn't work?
+            {
+                Exit();
+            }
 
             base.Update(gameTime);
         }
@@ -44,7 +128,13 @@ namespace Monogame_3___Loops__Lists__Input
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            foreach (Rectangle grass in grassTiles)
+            {
+                _spriteBatch.Draw(grassTexture, grass, Color.White);
+            }
+            _spriteBatch.Draw(mowerTexture, mowerRect, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
